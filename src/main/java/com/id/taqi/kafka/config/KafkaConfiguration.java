3a53d9735483cc4ad959d3b8821e912ed46d816a
= <=>
2020-09-26 05:41:41 +0700

    add config producer

--- src/main/java/com/id/taqi/kafka/config/KafkaConfiguration.java
+++ src/main/java/com/id/taqi/kafka/config/KafkaConfiguration.java
@@ -1,14 +1,15 @@
 package com.id.taqi.kafka.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,38 +17,49 @@ import java.util.Map;
 @Configuration
 public class KafkaConfiguration {
 
-//    @Value("${kafka.bootstrap-servers:localhost:9092}")
-//    private String bootstrapServers;
+    @Value("${spring.kafka.consumer.bootstrap-servers:localhost:9092}")
+    private String bootstrapServers;
 
     @Bean
-    public Map<String, Object> consumerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        // list of host:port pairs used for establishing the initial connections to the Kafka cluster
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        // allows a pool of processes to divide the work of consuming and processing records
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+    public ProducerFactory<String, String> producerFactory(){
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return  new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory(){
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
         // automatically reset the offset to the earliest offset
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         //auto commit offset
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         //interval for auto commit
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        config.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
 
-        return props;
+        return new  DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new StringDeserializer());
     }
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-    }
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
 
-    @Bean
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        return factory;
+        concurrentKafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
+
+        return concurrentKafkaListenerContainerFactory;
     }
 }
--- src/main/java/com/id/taqi/kafka/service/ConsumeService.java
+++ src/main/java/com/id/taqi/kafka/service/ConsumeService.java
@@ -1,8 +1,14 @@
 package com.id.taqi.kafka.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.id.taqi.kafka.assembler.MappingData;
+import com.id.taqi.kafka.modul.FpMasukanDto;
+import com.id.taqi.kafka.modul.InputReq;
+import com.id.taqi.kafka.modul.ListFpMasukanDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -14,14 +20,23 @@ import java.util.concurrent.CountDownLatch;
 @Service
 @Slf4j
 public class ConsumeService {
+    @Autowired
+    private KafkaTemplate template;
+
+    @Autowired
+    private MappingData mappingData;
     private final ObjectMapper mapper = new ObjectMapper();
     private CountDownLatch latch = new CountDownLatch(1);
 
-    @KafkaListener(topics = "kafka-test")
+    @KafkaListener(topics = "fpmasukan")
     public void consumData(@Payload String message,
                            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
                            @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) throws IOException {
-        log.info("{} in partition {}, receive {}", topic, partition, message);
+        log.info("{} in partition {}, consume {}", topic, partition, message);
         latch.countDown();
+
+
+        log.info("Sending to topic {}, with body {}", "kafka-test-produce", message);
+        template.send("kafka-test-produce",message);
     }
 }
